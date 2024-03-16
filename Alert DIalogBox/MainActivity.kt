ae4349a0e00e257973cf540954fa12424935cff2
Madhavendra-tripathi <madhavendratripathi3@gmail.com>
2024-03-16 18:13:42 +0530

    all

---
+++ Alert DIalogBox/MainActivity.kt
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:background="#EBF2F6"
+    android:gravity="center"
+    android:orientation="vertical"
+    tools:context=".MainActivity"
+    >
+
+    <TextView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Alert Dialog box example"
+        android:textSize="24dp"
+        android:textStyle="bold" />
+
+    <Button
+        android:layout_width="200dp"
+        android:layout_height="70dp"
+        android:layout_marginTop="20dp"
+        android:text="Click on it"
+        android:textSize="25dp"
+        android:id="@+id/btn"
+        />
+</LinearLayout>
